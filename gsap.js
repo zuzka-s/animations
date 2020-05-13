e7e9cf23c995fed7ac682bf1eb5415591a55f34e
Zuzana Sommerová <zuzana.sommerova@mall.cz>
2020-05-13 17:55:32 +0200

    scroll

--- gsap.js
+++ gsap.js
@@ -1,5 +1,22 @@
-let tl = gsap.timeline();
-tl.from( ".video-send", { duration: 2.5,
+TweenLite.defaultEase = Linear.easeNone;
+var controller = new ScrollMagic.Controller();
+var tl = new TimelineMax();
+tl.from(".video-send", 1.5, {
+    duration: 2.5,
     ease: "slow( 0.7, 0.7, false)",
     x: -800,
-    });
+});
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#animation-start",
+  duration: "100%",
+  triggerHook: 1
+})
+ /* .addIndicators({
+    name: "Box Timeline",
+    colorTrigger: "white",
+    colorStart: "white",
+    colorEnd: "white"
+  }) */
+  .setTween(tl)
+  .addTo(controller);
